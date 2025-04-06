1a6b6bc945b708cdeda79069f8549efe1f599262
Lucem-afferens <nikolaj.dudin.90@gmail.com>
2025-04-06 18:47:35 +0300

    final to send email and update styles

--- src/js/index.js
+++ src/js/index.js
@@ -46,47 +46,49 @@ window.addEventListener('scroll', () => {
       element.classList.add('hidden'); // Скрываем элемент
       element.classList.remove('visible'); // Скрываем элемент
     }
-  });
+});
 
 
 
-  // Инициализация EmailJS
-  (function () {
-    emailjs.init("YJ6wIebeKWvvTVwdl"); // вставь свой public key
-  })();
+// Инициализация EmailJS
+(function () {
+  emailjs.init("_L3Fwrf-tKc-jUOAI"); // вставь свой public key
+})();
 
-  document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
 
-    const policy = document.getElementById('policy');
-    if (!policy.checked) {
-      alert("Пожалуйста, примите политику конфиденциальности.");
-      return;
-    }
+  const policy = document.getElementById('policy');
+  if (!policy.checked) {
+    alert("Пожалуйста, примите политику конфиденциальности.");
+    return;
+  }
 
-    const loader = document.getElementById('form-loader');
-    const submitButton = this.querySelector('button[type="submit"]');
+  const loader = document.getElementById('form-loader');
+  const submitButton = this.querySelector('button[type="submit"]');
 
-    // Показать загрузку и заблокировать кнопку
-    loader.style.display = 'block';
-    submitButton.disabled = true;
-    submitButton.textContent = "Отправка...";
+  // Показать индикатор загрузки и блокировать кнопку
+  loader.style.display = 'flex';
+  submitButton.disabled = true;
+  submitButton.textContent = "Отправка...";
 
-    emailjs.sendForm('service_br8ubtx', 'template_wzmfe24', this)
-      .then(() => {
-        alert("Сообщение отправлено!");
-        this.reset();
-      })
-      .catch((error) => {
-        console.error("Ошибка отправки:", error);
-        alert("Произошла ошибка при отправке.");
-      })
-      .finally(() => {
-        loader.style.display = 'none';
-        submitButton.disabled = false;
-        submitButton.textContent = "Отправить сообщение";
-      });
-  });
+  emailjs.sendForm('service_c78rc3e', 'template_wzmfe24', this)
+    .then((response) => {
+      console.log("Успешно отправлено", response);
+      alert("Сообщение отправлено!");
+      this.reset();
+    })
+    .catch((error) => {
+      console.error("Ошибка отправки:", error);
+      alert("Произошла ошибка при отправке.");
+    })
+    .finally(() => {
+      submitButton.disabled = false; // Разблокировать кнопку
+      submitButton.textContent = "Отправить сообщение"; // Восстановить текст
+      loader.style.display = 'none'; // Скрыть индикатор
+    });
+});
+  
 
 
 
